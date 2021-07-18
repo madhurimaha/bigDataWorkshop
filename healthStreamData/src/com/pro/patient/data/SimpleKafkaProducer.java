fe454a80e453270ba1b7c46b70d4d00938e69237
madhurimaha <76150183+madhurimaha@users.noreply.github.com>
2021-07-18 11:25:22 +0530

    Add files via upload

---
+++ healthStreamData/src/com/pro/patient/data/SimpleKafkaProducer.java
@@ -0,0 +1,24 @@
+package com.pro.patient.data;
+
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class SimpleKafkaProducer {
+
+		public static void sendDataToKafkaSingleBroker(String msg, String topicName) {
+			Properties props = new Properties();
+			props.put("bootstrap.servers", "localhost:9092");
+			props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+			props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+			Producer<String, String> producer = new KafkaProducer<String, String>(props);
+			producer.send(new ProducerRecord<String, String>(topicName, "Doc1", msg));
+			producer.close();
+		
+
+	}
+
+}
